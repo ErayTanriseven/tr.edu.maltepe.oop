73df728397bfca9b5c581ae710ce31cd08752021
ErayTanriseven <eray_tanriseven08@hotmail.com>
2022-03-15 12:31:23 +0300

    Main

---
+++ src/C3.java
@@ -0,0 +1,3 @@
+public class C3 extends C2{
+    private int x = 3;
+}
---
+++ src/C4.java
@@ -0,0 +1,9 @@
+public class C4 extends C3{
+
+    private int x = 4;
+
+    
+    public int getX() {
+        return x;
+    }
+}
